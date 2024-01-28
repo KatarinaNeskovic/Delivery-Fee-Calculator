98a15c10699d42c54da73fd882f53042a13c3857
Katarina Neskovic <katarina.neskovic@gmail.com>
2024-01-28 20:33:01 +0100

    deleted extra comments

--- src/App.tsx
+++ src/App.tsx
@@ -10,32 +10,3 @@ function App() {
 
 export default App;
 
-
-/*
-  const [cartValue, setCartValue] = useState(0);
-  const [deliveryDistance, setDeliveryDistance] = useState(0);
-  const [amountOfItems, setAmountOfItems] = useState(0);
-  const [dateTime, setDateTime] = useState(new Date(2001, 0, 1)); //setting initial date to January 1st 2001
- 
-  const handleCartvalueChange = (e: any) => {
-    setCartValue(e.target.value);
-  }
- 
-  const handleDeliveryDistanceChange = (e: any) => {
-    setDeliveryDistance(e.target.value);
-  }
- 
-  const handleAmountOfItemsChange = (e: any) => {
-    setAmountOfItems(e.target.value);
-  }
- 
-  const handleDateTimeChange = (e: any) => {
-    setDateTime(e.target.value);
-  }
- */
-
-
-
-/*   <header> { className="App-header" }
-<img src={logo} className="App-logo" alt="logo" /> 
-</header> */
